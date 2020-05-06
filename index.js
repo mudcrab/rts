4f6aaf77284d5bd415ba12885cb8af9d6725c08c
Jevgeni Sillar <jevgeni@pitfire.io>
2020-05-07 01:31:51 +0300

    update react-ts version, update deps

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const latest = 'https://github.com/mudcrab/react-ts/archive/0.3.0.zip';
+const latest = 'https://github.com/mudcrab/react-ts/archive/0.4.0.zip';
 
 const stream = require('stream');
 const fs = require('fs');
